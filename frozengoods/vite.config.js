7fc870d9546fe2e928903a52abcfa0eb1122cc11
User <kobievillanueva26@gmail.com>
2025-04-02 11:18:44 +0800

    78

--- frozengoods/vite.config.js
+++ frozengoods/vite.config.js
@@ -5,7 +5,13 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.',
+  root: path.resolve(__dirname),
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html')
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
